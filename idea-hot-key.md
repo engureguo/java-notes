0e3c1b5e2c601ef2fd79816a4cf24bdbbfba5f8d
engureguo <8440193+engureguo@user.noreply.gitee.com>
2021-08-02 03:43:05 +0800

    hot keys, copyonwritelist

---
+++ idea-hot-key.md
@@ -0,0 +1,3 @@
+### idea hot keys
+- 清除无用的 import 语句 `ctrl alt o`
+- 格式化代码 `ctrl alt l`
--- src/com/engure/collection/list/Demo1.java
+++ src/com/engure/collection/list/Demo1.java
@@ -1,20 +1,23 @@
 package com.engure.collection.list;
 
 import org.junit.Test;
+
 import java.util.*;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 public class Demo1 {
     public static void main(String[] args) {
         List<Integer> list = new ArrayList<>();
 
         for (int i = 0; i < 30; i++) {
-            new Thread(()->{
-                for (int j=0; j<10; j++) {
+            new Thread(() -> {
+                for (int j = 0; j < 10; j++) {
                     list.add(new Random(System.currentTimeMillis()).nextInt()); // java.util.ConcurrentModificationException
                     System.out.println(list);
                 }
             }).start();
         }
+
 
     }
 
@@ -25,8 +28,8 @@ public class Demo1 {
         List<Integer> list = new Vector<>(); // List接口
 
         for (int i = 0; i < 30; i++) {
-            new Thread(()->{
-                for (int j=0; j<10; j++) {
+            new Thread(() -> {
+                for (int j = 0; j < 10; j++) {
                     list.add(new Random(System.currentTimeMillis()).nextInt());
                     System.out.println(list);
                 }
@@ -39,7 +42,7 @@ public class Demo1 {
         List<String> list = Collections.synchronizedList(new ArrayList<>());
 
         for (int i = 0; i < 30; i++) {
-            new Thread(()->{
+            new Thread(() -> {
                 list.add(UUID.randomUUID().toString().substring(0, 8));
                 System.out.println(list);
             }).start();
@@ -50,6 +53,25 @@ public class Demo1 {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+    }
+
+    @Test
+    public void test4() {
+        List<Object> list = new CopyOnWriteArrayList<>();
+
+        for (int i = 0; i < 30; i++) {
+            new Thread(() -> {
+                list.add(UUID.randomUUID().toString().substring(0, 8));
+                System.out.println(list);
+            }).start();
+        }
+
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
     }
 
 }
